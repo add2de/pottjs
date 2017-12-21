2ba9fee66ec0196cf6258121eb6be2036938772f
Pascal Küsgen <pascal@pascalmh.de>
2017-12-21 21:10:43 +0100

    fix: make stuff work again

--- js/main.js
+++ js/main.js
@@ -1,97 +1,107 @@
-(function (window, $) {
+var data = {},
+    filter,
+    loadedEntries = null;
 
-    var data = {},
-        filter,
-        entries = {};
+var sortBy = function (sortByKeyword) {
+    loadedEntries.sort(function(a, b) {
+        var contentA;
+        var contentB;
 
-    var sort = function (sortBy) {
-        container = document.querySelector('#container');
-        entries = container.querySelectorAll(".entry");
+        if (sortByKeyword == 'ups') {
+            contentA = a.data.ups;
+            contentB = b.data.ups;
+        } else if (sortByKeyword == 'downs') {
+            contentA = a.data.downs;
+            contentB = b.data.downs;
+        } else if (sortByKeyword == 'age') {
+            contentA = a.data.created;
+            contentB = b.data.created;
+        }
 
-        entries.sort(function(a,b){
-            var contentA = parseInt( $(a).data(sortBy));
-            var contentB = parseInt( $(b).data(sortBy));
-            return (contentA < contentB) ? -1 : 1;
-        });
+        return (contentA < contentB) ? -1 : 1;
+    });
+};
 
-        entries.forEach(function () {
-            container.append(this);
-        });
-    };
+var buildList = function () {
+    var container = document.querySelector('#container');
+    container.innerHTML = '';
 
-    var buildList = function (entries) {
-        var container = document.querySelector('#container');
-        var oldEntries = container.querySelectorAll('.entry');
+    loadedEntries.forEach(function (t) {
+        if(t.data.thumbnail == '' || t.data.thumbnail == 'self' ) return;
+        if(t.data.over_18) return;
+        if(t.data.stickied) return;
 
-        oldEntries.forEach(function(entry) {
-            container.removeChild(entry);
-        });
+        var entry = document.createElement('a');
+        entry.classList.add('entry');
+        entry.href = t.data.url;
+        entry.target = "_blank";
+        entry.setAttribute('data-score', t.data.score);
+        entry.setAttribute('data-ups', t.data.ups);
+        entry.setAttribute('data-downs', t.data.downs);
+        entry.setAttribute('data-created', t.data.created);
 
-        entries.forEach(function (t) {
-            if(t.data.thumbnail == '' || t.data.thumbnail == 'self' ) return;
-            if(t.data.over_18) return;
-            if(t.data.stickied) return;
+        var figure = document.createElement('figure');
+        figure.href = t.data.url;
+        figure.target = "_blank";
 
-            var entry = document.createElement('a');
-            entry.classList.add('entry');
-            entry.href = t.data.url;
-            entry.target = "_blank";
-            entry.setAttribute('data-score', t.data.score);
-            entry.setAttribute('data-ups', t.data.ups);
-            entry.setAttribute('data-created', t.data.created);
+        var thumbnail = document.createElement('img');
+        thumbnail.src = t.data.thumbnail;
+        figure.appendChild(thumbnail);
 
-            var figure = document.createElement('figure');
-            figure.href = t.data.url;
-            figure.target = "_blank";
+        var title = document.createElement('figcaption');
+        title.innerHTML = t.data.title;
+        figure.appendChild(title);
 
-            var thumbnail = document.createElement('img');
-            thumbnail.src = t.data.thumbnail;
-            figure.appendChild(thumbnail);
+        container.appendChild(entry).appendChild(figure);
+    });
 
-            var title = document.createElement('figcaption');
-            title.innerHTML = t.data.title;
-            figure.appendChild(title);
+};
 
-            container.appendChild(entry).appendChild(figure);
-        });
+var handleOnLoad = function (res) {
+    loadedEntries = res.target.response.data.children;
 
-    };
+    sortBy('ups');
+    buildList();
+};
 
-    var handleOnLoad = function (res) {
-        entries = res.target.response.data.children;
+var getJSON = function(sub, cat, limit) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onload = handleOnLoad;
+    httpRequest.responseType = 'json';
+    httpRequest.open(
+        'GET',
+        'https://www.reddit.com/r/'+sub+'/'+cat+'.json?g=GLOBAL&limit=' + limit +'',
+        true
+    );
+    httpRequest.send();
+};
 
-        buildList(entries);
-        //sort('ups'); // Funktioniert irgendwie nicht
-    };
+var init = function() {
+    var filter = document.querySelector('#filter');
+    filter.addEventListener('submit', function(e)  {
+        var formData = new FormData(filter),
+            subreddit = formData.get('subreddit'),
+            category = formData.get('category');
+            limit = formData.get('limit');
 
-    var getJSON = function(sub, cat, limit) {
-        var httpRequest = new XMLHttpRequest();
-        httpRequest.onload = handleOnLoad;
-        httpRequest.responseType = 'json';
-        httpRequest.open('GET', 'https://www.reddit.com/r/'+sub+'/'+cat+'.json', true);
-        httpRequest.send({ 'g': 'GLOBAL', limit: limit });
-    };
+        getJSON(subreddit, category, limit);
+        e.preventDefault();
+    });
 
-    var init = function() {
-        var filter = document.querySelector('#filter');
-        filter.addEventListener('submit', function(e)  {
-            var formData = new FormData(filter),
-                subreddit = formData.get('subreddit'),
-                category = formData.get('category');
-                limit = formData.get('limit');
+    document.querySelector('#sort').addEventListener('click', function(e)  {
+        var sortByCriteria = this.querySelector('input[name="sort"]:checked');
+        if (sortByCriteria && sortByCriteria.value) {
+            sortBy(sortByCriteria.value);
+            buildList();
+        }
+    });
 
-            getJSON(subreddit, category, limit);
-            e.preventDefault();
-        });
+    getJSON('memes', 'hot', '15');
+};
 
-        var sort = document.querySelector('#sort');
-        sort.addEventListener('click', function(e)  {
-            sort(this.querySelector('input[name="sort"]:checked').value);
-        });
-
-        getJSON('memes', 'hot', '15');
-    };
-
-    document.addEventListener('DOMContentLoaded', init());
-
-})(window);
+if (document.readyState !== 'loading') {
+    init();
+} else {
+    // the document hasn't finished loading/parsing yet so let's add an event handler
+    document.addEventListener('DOMContentLoaded', init);
+}
